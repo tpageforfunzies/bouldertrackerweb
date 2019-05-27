c3016fb4ec007d5f97bd2837607a53fbd520e4af
jhoot <jeffreydhooton@gmail.com>
2019-05-27 10:49:10 -0400

    add in conditional Beta selectors and star rating

--- src/components/pages/PostRoute.js
+++ src/components/pages/PostRoute.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Redirect, withRouter } from 'react-router-dom';
 import axios from 'axios';
 import StarRatings from 'react-star-ratings';
+import Select from 'react-select';
 
 import './scss/PostRoute.scss';
 
@@ -15,7 +16,43 @@ class PostRoute extends Component {
       id: this.props.id,
       error: false,
       goBack: false,
-      star_rating: 0
+      star_rating: 0,
+      numMoves: 0,
+      beta: [],
+      numMovesOptions: [
+        { value: 0, label: '0'},
+        { value: 1, label: '1'},
+        { value: 2, label: '2'},
+        { value: 3, label: '3'},
+        { value: 4, label: '4'},
+        { value: 5, label: '5'},
+        { value: 6, label: '6'},
+        { value: 7, label: '7'},
+        { value: 8, label: '8'},
+        { value: 9, label: '9'},
+        { value: 10, label: '10'}
+      ],
+      bodyPartOptions: [
+        { value: 'Left Hand', label: 'Left Hand'},
+        { value: 'Right Hand', label: 'Right Hand'},
+        { value: 'Left Foot', label: 'Left Foot'},
+        { value: 'Right Foot', label: 'Right Foot'}
+      ],
+      moveOptions: [
+        { value: 'Jug', label: 'Jug'},
+        { value: 'Slope', label: 'Slope'},
+        { value: 'Crimp', label: 'Crimp'},
+        { value: 'Pinch', label: 'Pinch'},
+        { value: 'Pocket', label: 'Pocket'},
+        { value: 'Bump', label: 'Bump'},
+        { value: 'Smear', label: 'Smear'},
+        { value: 'Edge', label: 'Edge'},
+        { value: 'Flag', label: 'Flag'},
+        { value: 'Toe Hook', label: 'Toe Hook'},
+        { value: 'Heel Hook', label: 'Heel Hook'},
+        { value: 'Drop Knee', label: 'Drop Knee'},
+        { value: 'Mantle', label: 'Mantle'} 
+      ]
     }
   }
 
@@ -70,7 +107,53 @@ class PostRoute extends Component {
     }
   }
 
+  handleNumMovesChange = option => {
+    this.setState({
+      numMoves: option.value
+    });
+    console.log('[handleNumMovesChange]', 'moves changed to '.concat(option.value).toString());
+  }
+
+  handleBodyPartOptionsChange = option => {
+    console.log('[handleBodyPartOptions]', option.value)
+  }
+
+  handleMoveOptionsChange = option => {
+    console.log('[handleMoveOptions]', option.value)
+  }
+
   render() {
+    console.log('rendering PostRoute');
+
+    let betaForm = [];
+
+    if(this.state.numMoves > 0) {
+      for(let i = 0; i < this.state.numMoves; i++) {
+        betaForm.push(
+          <div>
+            <p className="black bold">Move {i+1}:</p>
+            <div className="betaForm-container">
+              
+              <Select 
+                options={this.state.bodyPartOptions} 
+                className="bodypart-selector"
+                defaultValue={this.state.bodyPartOptions[0]}
+                onChange={this.handleBodyPartOptionsChange.bind(this)}
+              />
+              <Select 
+                options={this.state.moveOptions} 
+                className="move-selector"
+                defaultValue={this.state.moveOptions[0]}
+                onChange={this.handleMoveOptionsChange.bind(this)}
+              />
+            </div>
+          </div>
+        )
+      }
+    } else {
+      betaForm.push(<p className="bold">Select number of moves to enter beta</p>);
+    }
+
     if (this.props.id > 0) {
       if (this.state.goBack) {
         this.props.history.push('/profile');
@@ -93,7 +176,7 @@ class PostRoute extends Component {
               onChange={this.handleGradeChange.bind(this)}
               value={this.state.password} 
             />
-            <p className="bold">Your rating:</p>
+            <h3 className="bold black">Your rating:</h3>
             <StarRatings
               rating={this.state.star_rating}
               starRatedColor="#B60B31"
@@ -101,10 +184,20 @@ class PostRoute extends Component {
               numberOfStars={5}
               name='rating'
             />
+            <h3 className="bold black">Beta(optional):</h3>
+            <p className="bold black">Select number of moves</p>
+            <Select 
+              options={this.state.numMovesOptions} 
+              className="moves-selector"
+              defaultValue={this.state.numMovesOptions[0]}
+              onChange={this.handleNumMovesChange.bind(this)}
+            />
+            {betaForm}
             <input 
               type="submit" 
               value="Submit" 
             />
+
           </form>
         </div>
       );
