29d631e60419c134b89228fd6ec6b39bb722960a
jhoot <jeffreydhooton@gmail.com>
2019-05-31 17:48:37 -0400

    idk

--- src/components/general/RouteBox.js
+++ src/components/general/RouteBox.js
@@ -1,39 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Link } from 'react-router-dom';
 import CommentAccordion from './CommentAccordion';
 
 import './scss/RouteBox.scss';
 
-function RouteBox(props) {
-  var routeurl = '/route/'.concat(props.routeid);
-  let sendDate = new Date(props.sendDate);
-  let comment;
+class RouteBox extends Component {
+  constructor(props) {
+    super(props);
 
-  if(props.comments) {
-    if(props.comments.length) {
-      comment = <CommentAccordion comments={props.comments}/>;
+    this.state = {
+      routeurl: '/route/'.concat(this.props.routeid),
+      sendDate: new Date(this.props.sendDate)
+    }
+  }
+
+  render() {
+    let comment;
+    if(this.props.comments) {
+      if(this.props.comments.length) {
+        comment = <CommentAccordion comments={this.props.comments}/>;
+      } else {
+        comment = <div></div>;
+      }
     } else {
       comment = <div></div>;
     }
-  } else {
-    comment = <div></div>;
-  }
 
-  console.log('[RouteBox] props', props);
-
-  return (
-    <div className="single-box uk-width-1-1 uk-width-1-3@s">
-        <div className="container">
-          <img src="https://placekitten.com/400/300" alt="placeholder route" />
-          <div className="text">
-            <NavLink className="bold" jwt={props.jwt} userID={props.id}>{props.name}</NavLink>
-            <h4>V{props.grade}</h4>
-            <h4>{sendDate.toDateString()}</h4>
-            {comment}
+    return (
+      <div className="single-box uk-width-1-1 uk-width-1-3@s">
+          <div className="container">
+            <img src="https://placekitten.com/400/300" alt="placeholder route" />
+            <div className="text">
+              <Link className="bold routelink" jwt={this.props.jwt} userID={this.props.id} to={this.state.routeurl}>{this.props.name}</Link>
+              <h4>V{this.props.grade}</h4>
+              <h4>{this.state.sendDate.toDateString()}</h4>
+              {comment}
+            </div>
           </div>
-        </div>
-    </div>
-  );
+      </div>
+    );
+  }
+  
 }
 
 export default RouteBox;
