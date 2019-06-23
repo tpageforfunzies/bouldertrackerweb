9f854a0e9aacb8dc1b6ff18a1bd841784a181e8b
jhoot <jeffreydhooton@gmail.com>
2019-06-23 14:01:58 -0400

    add offcanvas on not authed

--- src/components/general/Header.js
+++ src/components/general/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, withRouter } from 'react-router-dom';
 
 import './scss/Header.scss';
+import bars from '../../assets/images/bars.png';
 
 class Header extends Component  {
 
@@ -41,6 +42,45 @@ class Header extends Component  {
                 </li>
               </ul>       
             </div>
+            <div className="offcanv-toggle">
+              <a href="#offcanv" data-uk-toggle>
+                <img src={bars} />
+              </a>
+            </div>
+            <div id="offcanv" data-uk-offcanvas="flip: true">
+              <div class="uk-offcanvas-bar">
+                <div className="uk-grid uk-grid-collapse">
+                  <div className="uk-width-2-3">
+                    <h2 className="white bold">Boulder<br />Tracker</h2>
+                  </div>
+                  <div className="uk-width-1-3">
+                    <button class="uk-offcanvas-close" type="button" data-uk-close></button>    
+                  </div>
+                </div>
+                <div className="menu">
+                  <ul>
+                    <li>
+                      <Link to="/eventually/dir" className="white bold">Directory</Link>
+                    </li>
+                    <li>
+                      <Link to="/eventually/fc" className="oc-bt">Followed Climbers</Link>
+                    </li>
+                    <li>
+                      <Link to="/new-route" className="oc-bt">Add Route</Link>
+                    </li>
+                    <li>
+                      <Link to="/eventually/hoc" className="oc-bt">Hall of Crush</Link>
+                    </li>
+                    <li>
+                      <Link to="/profile" className="oc-bt">My Profile</Link>
+                    </li>
+                    <li>
+                      <a className="oc-bt" onClick={this.props.handleLogout}>Logout</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="header-spacer"></div>
         </div>
@@ -67,6 +107,35 @@ class Header extends Component  {
               <Link to="/register" className="button white-outline">Register</Link>
             </li>
           </ul>       
+        </div>
+        <div className="offcanv-toggle">
+          <a href="#offcanv" data-uk-toggle="flip: true;">
+            <img src={bars} />
+          </a>
+        </div>
+        <div id="offcanv" data-uk-offcanvas>
+          <div class="uk-offcanvas-bar">
+              <div className="uk-grid uk-grid-collapse">
+                <div className="uk-width-2-3">
+                  <h2 className="white bold">Boulder<br />Tracker</h2>
+                </div>
+                <div className="uk-width-1-3">
+                  <button class="uk-offcanvas-close" type="button" data-uk-close></button>    
+                </div>
+              </div>
+              <div className="menu">
+                <ul>
+                  <li>
+                    <Link to="/login" className="oc-bt">Login</Link>
+                  </li>
+                  <li>
+                    <Link to="/register" className="oc-bt">Register</Link>
+                  </li>
+                </ul> 
+              </div>
+              
+
+          </div>
         </div>
       </div>
     );
